665981bca1485658b715ce213ccaef87b5774e84
subbu7788 <subbu7788@live.in>
2023-02-02 18:29:18 +0530

    Update picker.js

--- JS/picker.js
+++ JS/picker.js
@@ -1,12 +1,6 @@
 document.addEventListener("DOMContentLoaded", () =>{
  
-// Register service worker to control making site work offline
 
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker
-    .register('/Dequecca/serviceworker.js')
-    .then(() => { console.log('Service Worker Registered'); });
-}
 // Code to handle install prompt on desktop
 
 let deferredPrompt;
